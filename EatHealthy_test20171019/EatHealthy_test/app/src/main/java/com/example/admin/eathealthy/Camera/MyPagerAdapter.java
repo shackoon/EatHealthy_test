77a04f90a1a0263ad7239436a935830780be436e
shackoon <a0930210276@gmail.com>
2017-10-25 20:11:38 +0800

    20171025增加圖片頁數

--- EatHealthy_test20171019/EatHealthy_test/app/src/main/java/com/example/admin/eathealthy/Camera/MyPagerAdapter.java
+++ EatHealthy_test20171019/EatHealthy_test/app/src/main/java/com/example/admin/eathealthy/Camera/MyPagerAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
@@ -44,6 +45,8 @@ public class MyPagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.content_trans, container, false);
         ImageView imageView = view.findViewById(R.id.img_for_trans);
+//        TextView tv_page = view.findViewById(R.id.tv_page);
+//        tv_page.setText(position + 1 + "/" + arrayPic.size());
         Glide.with(imageView.getContext()).load(arrayPic.get(position).getImage()).fitCenter().into(new GlideDrawableImageViewTarget(imageView) {
             @Override
             public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
